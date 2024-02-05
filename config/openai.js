635b2d22f3be8c8ed793e4bf30b8b097846538fb
Roman Timashoff <timashoff@gmail.com>
2024-02-06 04:04:23 +0800

    feat: simple chat implementation in color

--- config/openai.js
+++ config/openai.js
@@ -1,5 +1,8 @@
 import OpenAI from 'openai';
+import * as readline from 'node:readline/promises'
+import { stdin as input, stdout as output } from 'node:process'
 
-export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const rl = readline.createInterface({ input, output })
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY, })
+
+export { rl, openai }
--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-import { openai } from "./config/openai.js"
+import { openai, rl } from "./config/openai.js"
 
 async function main() {
-  const chatCompletion = await openai.chat.completions.create(
-    {
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: 'Say this is a test' }],
+
+  while (true) {
+    const userInput = await rl.question('\n\x1b[32m> ')
+
+    if (userInput.toLowerCase() === 'exit') {
+      rl.close()
+      return
     }
-  )
 
-  console.log('ai:', chatCompletion.choices[0].message.content)
-  console.log('tokens$', chatCompletion.usage.total_tokens)
+    if (!userInput || userInput.trim().split(' ').length < 3) {
+      console.error('\u001B[91mthe input must not be empty or shorter than 3 words')
+      continue
+    }
+
+    try {
+      const stream = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo-0125',
+        messages: [{ role: 'user', content: userInput }],
+        stream: true,
+      })
+      console.time('time to respond')
+      for await (const chunk of stream) {
+        process.stdout.write(chunk.choices[0]?.delta?.content || '\n\x1b[0m')
+      }
+      console.timeEnd('time to respond')
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
 }
-
 main()
