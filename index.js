0a2e3b51187ccc767a9559cda14c3399e354900a
Roman Timashoff <timashoff@gmail.com>
2024-02-06 18:33:45 +0800

    feat: implementation of a short history context

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { openai, rl } from "./config/openai.js"
 
 async function main() {
 
+  const chatHistory = []
+
   while (true) {
     const userInput = await rl.question('\n\x1b[32m> ')
 
@@ -10,26 +12,38 @@ async function main() {
       return
     }
 
-    if (!userInput || userInput.trim().split(' ').length < 3) {
+    if (!userInput || userInput.trim().split(' ').length < 2) {
       console.error('\u001B[91mthe input must not be empty or shorter than 3 words')
       continue
     }
 
     try {
+      const messages = chatHistory.map(([role, content]) => ({ role, content, }))
+      messages.push({ role: 'user', content: userInput })
+
       const stream = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo-0125',
-        messages: [{ role: 'user', content: userInput }],
+        messages,   // messages: [{ role: 'user', content: userInput }],
         stream: true,
       })
       console.time('time to respond')
+      const response = []
       for await (const chunk of stream) {
-        process.stdout.write(chunk.choices[0]?.delta?.content || '\n\x1b[0m')
+        const content = chunk.choices[0]?.delta?.content
+        if (content) response.push(content)
+        process.stdout.write(content || '\n\x1b[0m')
       }
       console.timeEnd('time to respond')
 
-    } catch (error) {
-      console.error(error)
+      chatHistory.push(['user', userInput], ['assistant', response.join('')])
+
+      if (chatHistory.length > 6) {
+        chatHistory.splice(0, 2)
+      }
+
+      console.log(chatHistory.length)
     }
+    catch (error) { console.error(error) }
   }
 
 }
